ee5ab437ea522f430fca0b837485352b26c8e152
Sheetal Jain <jsheetal756@gmail.com>
2025-07-23 22:13:34 +0530

    Create evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
+    print(f"Mean Squared Error (MSE): {mse:.2f}")
+    print(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
+    print(f"R² Score: {r2:.2f}")
